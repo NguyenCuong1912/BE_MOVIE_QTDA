5171d324011d15d36d64da536dfd1c1285cdc136
CuongNguyen <Cuongn1912@gamil.com>
2022-06-07 20:27:31 +0700

    add room

--- routers/index.js
+++ routers/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const { cinemasRouter } = require('./Cinema.router');
+const { filmsRouter } = require('./Film.routers');
+const { groupCinemasRouter } = require('./GroupCinema.router');
+const { roomRouter } = require('./Room.router');
 const { TypeUserRouter } = require('./TypeUser.routers');
 const { userRouter } = require('./User.routers');
 
 const rootRouter = express.Router();
 rootRouter.use('/typeUsers', TypeUserRouter);
 rootRouter.use('/users', userRouter);
+rootRouter.use('/films', filmsRouter);
+rootRouter.use('/cinemas', cinemasRouter);
+rootRouter.use('/groupCinemas', groupCinemasRouter);
+rootRouter.use('/rooms', roomRouter);
+
+
 
 module.exports = {
     rootRouter
