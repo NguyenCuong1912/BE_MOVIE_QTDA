08a42b38eb086de9d7c04922cb0de95c53517720
CuongNguyen <Cuongn1912@gamil.com>
2022-06-07 20:38:13 +0700

    create seat

--- routers/index.js
+++ routers/index.js
@@ -5,6 +5,8 @@ const { groupCinemasRouter } = require('./GroupCinema.router');
 const { roomRouter } = require('./Room.router');
 const { TypeUserRouter } = require('./TypeUser.routers');
 const { userRouter } = require('./User.routers');
+const { seatRouter } = require('./Seat.router');
+
 
 const rootRouter = express.Router();
 rootRouter.use('/typeUsers', TypeUserRouter);
@@ -13,6 +15,7 @@ rootRouter.use('/films', filmsRouter);
 rootRouter.use('/cinemas', cinemasRouter);
 rootRouter.use('/groupCinemas', groupCinemasRouter);
 rootRouter.use('/rooms', roomRouter);
+rootRouter.use('/seats', seatRouter);
 
 
 
